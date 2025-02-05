addea742dee238bf4fe014d64568c905e75fb92d
adilzhanshukenov <shukenovadilzhan@gmail.com>
2025-02-05 18:02:55 +0500

    vite-config

--- green-api-wa/vite.config.ts
+++ green-api-wa/vite.config.ts
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './', // Ensures assets load correctly
-});
+  plugins: [react()],
+  server: {    proxy: {"/api": "http://localhost:5000", // Example backend URL   
+     },  
+  },
+})
